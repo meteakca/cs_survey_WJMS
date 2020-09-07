8e533f539dcfba70bfe9a8fbd7000c5ac68598c3
Mete Akcaoglu <mete.akca@gmail.com>
2020-09-07 14:24:53 -0400

    submitted version

--- AECT2018_Analyses.R
+++ AECT2018_Analyses.R
@@ -1,5 +1,5 @@
-#initiate the packages
-  library(tidyr)#
+#load the packages
+  library(tidyr)
   library(ggplot2)
   library(tidyverse)
   library(RColorBrewer)
@@ -12,7 +12,19 @@
   library(hrbrthemes)
   library(ggthemes)
   library(jtools)
-  
+  library(huxtable)
+  library(psych)
+#Descriptives
+
+  ggplot(data = subset(df_ss, !is.na(df_ss$ProgramingExtent)), aes(x = as.factor(ProgramingExtent)))+
+    geom_bar(aes(fill=ProgramingExtent), show.legend = FALSE)+
+    scale_x_discrete(labels = c('None','A little','Some','A lot'))+
+    labs(x="", y="Count",
+         title="",
+         subtitle="",
+         caption="") +
+    theme(text = element_text(size=20))
+
 #Impact of test and its interaction with Gender and Programming Extent: RQ1, 1a, 1b
 #RQ1: Does taking the test impact students' perceptions? No
 df_ss %>%
@@ -25,9 +37,12 @@ df_ss %>%
       y = Means,
       fill = as.factor(Test)
     )) +
-    facet_wrap(Mot ~ ., ncol = 1) +
-    theme_ipsum() +
-    ggtitle("Impact of taking the test on motivation outcomes")
+    #facet_wrap(Mot ~ ., ncol = 1) +
+  labs(x="Experimental group (0 = Control, 1 = Test)", y="Motivation Mean (1 to 7)",
+       title="Impact of taking the test on motivation outcomes",
+       subtitle="A plot that is only useful for demonstration purposes",
+       caption="Brought to you by the letter 'g'") +
+  theme_ipsum()
 
  lm1 <- lm(Int_Mean_1R ~ Test, data = df_ss)
   summary(lm1)
@@ -41,14 +56,53 @@ df_ss %>%
     drop_na() %>%
     ggplot(aes(y = Means, x = as.factor(Test))) +
     geom_boxplot(aes(fill = Gender)) +
-    facet_wrap(Mot ~ ., ncol = 1) +
     facet_wrap(Mot ~ .,
-               ncol = 1,
-               labeller = labeller(key = labels_outcome)) +
+               ncol = 1) +
     theme_ipsum_ps() +
     ylab("Outcome Levels") +
     xlab("No Test Condition ------- Test Condition") +
     ggtitle("Interaction of Test and Gender on \nMotivation Outcomes")
+  
+  #RQ1a: Tests performance interaction with Gender.
+  df_ss %>%
+    pivot_longer(cols = ends_with("Mean_1"),
+                 names_to = "Mot",
+                 values_to = "Means") %>%
+    drop_na() %>%
+    ggplot(aes(y = Means, x = DBN_POMP)) +
+    geom_point(aes(color = Gender)) +
+    facet_wrap(Mot ~ .,
+               ncol = 1) +
+    theme_ipsum_ps() +
+    ylab("Outcome Levels") +
+    xlab("No Test Condition ------- Test Condition") +
+    ggtitle("Interaction of Test and Gender on \nMotivation Outcomes")
+    
+#RQ3 Gender performance interaction - self-efficacy
+  df_ss <- df_ss %>%
+    filter(!is.na(Gender))
+  ggplot(df_ss) +
+    aes(x = Exp_Mean_1, y = DBN_POMP, colour = Gender) +
+    geom_jitter(size = 4.96, alpha = 0.8) +
+    geom_smooth(method = lm, se = FALSE) +
+    scale_colour_grey() +
+    labs(x = "Self-efficacy", y = "Pretest Performance") +
+    theme_minimal() +
+    theme(text = element_text(size = 20))
+
+#RQ3 Gender performance interaction - UV
+  df_ss <- df_ss %>%
+    filter(!is.na(Gender))
+  ggplot(df_ss) +
+    aes(x = Val_Mean_1, y = DBN_POMP, colour = Gender) +
+    geom_jitter(size = 4.96, alpha = 0.7) +
+    #geom_boxplot()+
+    geom_smooth(method = lm, se = FALSE) +
+   # scale_colour_grey() +
+    labs(x = "Utility Value", y = "Pretest Performance") +
+    theme_minimal() +
+    theme(text = element_text(size = 20))  
+  
 
 #Regression for gender X test interaction 
 #Interest not significant
@@ -69,9 +123,31 @@ df_ss %>%
 
   plot_summs(lm1a, lm1a2, lm1a3, plot.distributions = TRUE)
   export_summs(lm1a, lm1a2, lm1a3, to.file = "docx", file.name = "test.docx", pvals = TRUE)
-
+  genderTestregs <-
+    huxreg(
+      'Interest' = lm1a,
+      'Value' = lm1a2,
+      'Self-efficacy' = lm1a3,
+      bold_signif = 0.05
+    )
+  genderTestregs                                                         %>%
+    theme_article                                                  %>%
+    set_background_color(1:nrow(genderTestregs), evens, grey(.95)) %>%
+    set_font_size(final(), 1, 9)                                   %>%
+    set_bold(final(), 1, FALSE)                                    %>%
+    set_top_border(final(), 1, 1)                                  %>%
+    set_caption('Linear regressions of motivation outcomes')      %>%
+    quick_pdf(
+      genderTestregs,
+      file = "huxtable-output.pdf",
+      borders = 0.4,
+      open = interactive(),
+      width = NULL,
+      height = NULL
+    )
+  
 #RQ1b: Tests interaction with Programming Experience
-  p <- df_ss %>%
+df_ss %>%
     pivot_longer(cols = contains("Mean_1"),
                  names_to = "Mot",
                  values_to = "Means") %>%
@@ -85,7 +161,6 @@ df_ss %>%
     xlab("No Test Condition ------- Test Condition") +
     ggtitle("Interaction of Programming Extent and Test on \nMotivation Outcomes")
 
-  
 #Regression for programming extent X test interaction
 #Interest not significant
   lm1b <-
@@ -168,3 +243,199 @@ summ(lm2_2)
   lm2b_3 <- lm(Exp_Mean_1 ~ ProgramingExtent + Gender + Gender * ProgramingExtent, data = df_ss)
   summ(lm2b_3)
   interact_plot(lm2b_3, pred = ProgramingExtent, modx = Gender)
+  
+
+  
+#Analysis for the test-takers, to see the impact of taking the test:
+#DBN_POMP and motivation outcomes
+  df_ss %>%
+    pivot_longer(cols = contains("Mean_1"),
+                 names_to = "Mot",
+                 values_to = "Means") %>%
+    ggplot(aes(x = DBN_POMP,
+               y = Means)) +
+    geom_boxplot(aes(color = DBN_POMP)) +
+    facet_wrap(Mot ~ ., ncol = 1) +
+    theme_minimal() +
+    ggtitle("Impact of taking the test on motivation outcomes")
+
+#interst
+lm3 <- lm(Int_Mean_1R ~ DBN_POMP, data = df_ss)
+summ(lm3)
+#value
+lm3a <- lm(Val_Mean_1 ~ DBN_POMP, data = df_ss)
+summ(lm3a)
+#expecanties
+lm3b <- lm(Exp_Mean_1 ~ DBN_POMP, data = df_ss)
+summ(lm3b)  
+
+
+#DBN_POMP +Control and interactions
+#first gender (filtering high pomp scores)
+df_ss %>%
+  filter(DBN_POMP<60) %>% 
+  pivot_longer(cols = contains("Mean_1"),
+               names_to = "Mot",
+               values_to = "Means") %>%
+  drop_na() %>% 
+  ggplot(aes(x = DBN_POMP,
+             y = Means)) +
+  geom_boxplot(aes(fill = as.factor(Gender), color = as.factor(Gender))) +
+  facet_wrap(Mot ~ ., ncol = 1) +
+  theme_minimal() +
+  ggtitle("Impact of taking the test on motivation outcomes")
+
+#interst
+lm3_1 <-
+  lm(Int_Mean_1R ~ DBN_POMP + Gender + DBN_POMP * Gender, data = df_ss)
+summ(lm3_1)
+plot_model(lm3_1, type = "int")
+interact_plot(lm3_1, pred = DBN_POMP, modx = Gender)
+
+#value
+lm3_1a <-
+  lm(Val_Mean_1 ~ DBN_POMP + Gender + DBN_POMP * Gender, data = df_ss)
+summ(lm3_1a)
+plot_model(lm3_1a, type = "int")
+interact_plot(lm3_1a, pred = DBN_POMP, modx = Gender)
+
+#expecanties
+lm3_1b <-
+  lm(Exp_Mean_1 ~ DBN_POMP + Gender + DBN_POMP * Gender, data = df_ss)
+summ(lm3_1b)
+plot_model(lm3_1b, type = "int")
+interact_plot(lm3_1b, pred = DBN_POMP, modx = Gender)
+
+#programming experience
+df_ss %>%
+  pivot_longer(cols = contains("Mean_1"),
+               names_to = "Mot",
+               values_to = "Means") %>%
+  drop_na() %>% 
+  ggplot(aes(x = ProgramingExtent,
+             y = Means)) +
+  geom_boxplot(aes(fill = ProgramingExtent)) +
+  facet_wrap(Mot ~ ., ncol = 1) +
+  theme_minimal() +
+  ggtitle("Impact of taking the test on motivation outcomes")
+
+#interst
+lm3_2 <-
+  lm(Int_Mean_1R ~ DBN_POMP + ProgramingExtent + DBN_POMP * ProgramingExtent, data = df_ss)
+summ(lm3_2)
+plot_model(lm3_2, type = "int")
+interact_plot(lm3_2, pred = DBN_POMP, modx = ProgramingExtent)
+
+#value
+lm3_2a <-
+  lm(Val_Mean_1 ~ DBN_POMP  + ProgramingExtent + DBN_POMP * ProgramingExtent, data = df_ss)
+summ(lm3_2a)
+plot_model(lm3_2a, type = "int")
+interact_plot(lm3_2a, pred = DBN_POMP, modx = ProgramingExtent)
+
+#expecanties
+lm3_2b <-
+  lm(Exp_Mean_1 ~ DBN_POMP  + ProgramingExtent + DBN_POMP * ProgramingExtent, data = df_ss)
+summ(lm3_2b)
+plot_model(lm3_2b, type = "int")
+interact_plot(lm3_2b, pred = DBN_POMP, modx = ProgramingExtent)
+
+
+
+#programming experience + Gender x DBNPomp (3-way interaction)
+df_ss %>%
+  pivot_longer(cols = contains("Mean_1"),
+               names_to = "Mot",
+               values_to = "Means") %>%
+  drop_na() %>% 
+  ggplot(aes(x = DBN_POMP,
+             y = Means)) +
+  geom_boxplot(aes(color = Gender)) +
+  facet_wrap(Mot ~ ProgramingExtent, ncol = 4) +
+  theme_minimal() +
+  ggtitle("Impact of taking the test on motivation outcomes")
+
+#interst -continue here
+lm3_3 <-
+  lm(Int_Mean_1R ~ DBN_POMP + ProgramingExtent + Gender+ DBN_POMP * ProgramingExtent * Gender, data = df_ss)
+summ(lm3_3)
+plot_model(lm3_3, type = "pred")
+interact_plot(lm3_3, pred = DBN_POMP, modx = ProgramingExtent)
+
+#value
+lm3_3a <-
+  lm(Val_Mean_1 ~ DBN_POMP  + ProgramingExtent + Gender+ DBN_POMP * ProgramingExtent * Gender, data = df_ss)
+summ(lm3_3a)
+plot_model(lm3_3a, type = "int")
+interact_plot(lm3_3a, pred = DBN_POMP, modx = ProgramingExtent)
+
+#expecanties
+lm3_3b <-
+  lm(Exp_Mean_1 ~ DBN_POMP  + ProgramingExtent + Gender+ DBN_POMP * ProgramingExtent * Gender, data = df_ss)
+summ(lm3_3b)
+plot_model(lm3_3b, type = "int")
+interact_plot(lm3_3b, pred = DBN_POMP, modx = ProgramingExtent)
+
+
+
+
+
+#using the DBNandNoTest Variable
+#DBNandNoTest ~ Interest - sig
+lm4 <-
+  lm(Int_Mean_1R ~ PerfAndControl, data = df_ss)
+summ(lm4)
+glance(lm4)
+plot_model(lm4, type = "pred")
+
+df_ss %>%
+    pivot_longer(cols = contains("Mean_1"),
+                 names_to = "Mot",
+                 values_to = "Means") %>%
+    ggplot(aes(y = Means, x = PerfAndControl)) +
+    geom_boxplot(aes(fill = PerfAndControl)) +
+    facet_wrap(Mot ~ ., ncol = 1) +
+    theme_minimal() +
+    ylab("Outcome Levels") +
+    ggtitle("")
+
+#anova for Tukey post hoc
+aov1 <- aov(Int_Mean_1R ~ PerfAndControl, data = df_ss)
+summary.aov(aov1)
+
+#value - sig
+lm4a <-
+  lm(Val_Mean_1 ~ PerfAndControl, data = df_ss)
+summ(lm4a)
+glance(lm4a)
+plot_model(lm4a, type = "pred")
+
+#exp - not sig
+lm4b <-
+  lm(Exp_Mean_1 ~ PerfAndControl, data = df_ss)
+summ(lm4b)
+glance(lm4b)
+plot_model(lm4b, type = "pred")
+
+#interaction with gender - interest - significant
+lm4_1 <-
+  lm(Int_Mean_1R ~ PerfAndControl + Gender + Gender * PerfAndControl, data = df_ss)
+summ(lm4_1)
+glance(lm4_1)
+plot_model(lm4_1, type = "int")
+
+#value not sign
+lm4_1a <-
+  lm(Val_Mean_1 ~ PerfAndControl + Gender + Gender * PerfAndControl, data = df_ss)
+summ(lm4_1a)
+glance(lm4_1a)
+plot_model(lm4_1a, type = "int")
+
+#Expectancy not sign
+lm4_1b <-
+  lm(Exp_Mean_1 ~ PerfAndControl + Gender + Gender * PerfAndControl, data = df_ss)
+summ(lm4_1b)
+glance(lm4_1b)
+plot_model(lm4_1b, type = "int")
+
+huxreg(lm4_1, lm4_1a, lm4_1b)
--- supplementaryanalysis.R
+++ supplementaryanalysis.R
@@ -12,15 +12,29 @@ df_ss$Int_Mean_1R[122] <- 4
 df_ss$Int_Mean_1R[123] <- 5.6
 df_ss$Int_Mean_1R[146] <- 3
 
-
 hist(df_ss$Int_Mean_1R)
 hist(df_ss$Val_Mean_1)
 hist(df_ss$Exp_Mean_1)
 
 
-df_ss$Gender <- factor(df_ss$Gender,
-                       levels = c(0, 1),
-                       labels = c("Male", "Female"))
+df_ss$DBNandNoTest[211] <- "highperformance"
+df_ss$DBNandNoTest[212] <- "highperformance"
+df_ss$DBNandNoTest[213] <- "highperformance"
+df_ss$DBNandNoTest[214] <- "highperformance"
+df_ss$DBNandNoTest[215] <- "highperformance"
+df_ss$DBNandNoTest[216] <- "highperformance"
+
+df_ss[df_ss$DBNandNoTest == "notest", "DBNandNoTest"] <- 0
+df_ss[df_ss$DBNandNoTest == "lowperformance", "DBNandNoTest"] <- 1
+df_ss[df_ss$DBNandNoTest == "moderateperformance", "DBNandNoTest"] <- 2
+df_ss[df_ss$DBNandNoTest == "highperformance", "DBNandNoTest"] <- 3
+
+
+df_ss$Int_Mean_1R <- as.numeric(df_ss$Int_Mean_1R)
+df_ss$Int_Mean_1 <- as.numeric(df_ss$Int_Mean_1)
+df_ss$Val_Mean_1 <- as.numeric(df_ss$Val_Mean_1)
+df_ss$Exp_Mean_1 <- as.numeric(df_ss$Exp_Mean_1)
+df_ss$ProgramingExtent <- as.numeric(df_ss$ProgramingExtent)
 
 
 df_ss$ProgramingExtent <- factor(
@@ -35,8 +49,36 @@ df_ss$Test <- factor(df_ss$Test,
 
 
 df_ss$ProgramingExtent <- as.numeric(df_ss$ProgramingExtent)
+df_ss$DBN_POMP <- as.numeric(df_ss$DBN_POMP)
+df_ss$PerfAndControl <- as.factor(df_ss$PerfAndControl)
+
 
 df_ss$Test <- as.numeric(df_ss$Test)
 df_ss$Test[df_ss$Test == "NA"] <- "0"
 df_ss$Test[df_ss$Test == "Test"] <- "1"
 df_ss$Test[is.na(df_ss$Test)] <- 0
+
+#cleaning up the environment, removing old models
+rm(lm3, lm3a, lm3b, m1, m2, m3, m4, m5, m5a, m5b, m6, m6a, m6b, p, lm3c) 
+
+#create a new variable
+df_ss$PerfAndControl <- ifelse(df_ss$DBN_POMP<18, 1, 2)
+df_ss$PerfAndControl[is.na(df_ss$PerfAndControl)] <- 0
+
+summary(df_ss)
+DataExplorer::plot_bar(df_ss)
+
+write.csv(df_ss, file = "surveyCS.csv")
+
+
+
+#reliability of the pretest
+df_ss$DBN1 <-  as.numeric(df_ss$DBN1)
+df_ss$DBN2 <-  as.numeric(df_ss$DBN2)
+df_ss$DBN3 <-  as.numeric(df_ss$DBN3)
+df_ss$DBN4 <-  as.numeric(df_ss$DBN4)
+df_ss$DBN5 <-  as.numeric(df_ss$DBN5)
+df_ss$DBN6 <-  as.numeric(df_ss$DBN6)
+
+pretest <- select(df_ss, 6,7,8,9,10,11)
+alpha(pretest) 
